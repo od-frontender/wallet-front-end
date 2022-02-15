7aa0a4bbd8f5d0b911a10513b008559b5de820e9
Vladomira <juliavladomira@gmail.com>
2022-02-15 23:27:29 +0300

    corrects total balance

--- src/redux/functionServise.js
+++ src/redux/functionServise.js
@@ -2,15 +2,12 @@ import dateFormat from 'dateformat';
 
 export function userBalanсe(payload) {
   const data = payload.map(({ total, createdAt }) => {
-    return { total, createdAt: dateFormat(createdAt, 'mm.dd.yy') };
+    return { total, createdAt: dateFormat(createdAt, 'mm.dd.yy h:MM:ss ') };
   });
 
   const largestData = data.sort((a, b) => {
     return Date.parse(b.createdAt) - Date.parse(a.createdAt);
   });
-  console.log(largestData);
-
   const result = largestData[0].total;
-  console.log('result', result);
   return result;
 }
