d3d36f19713986f252db407c0b022615d53d888e
od-frontender <od.frontender@gmail.com>
2022-02-15 12:31:02 +0200

    исправил ошибку

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-
 import MainView from './views/MainView';
 import './styles/index.scss';
+import AppBar from './components/AppBar/AppBar';
 
 import Currency from './components/Currency';
 import CategoryList from './components/Statistic/Category/CategoryList';
 import TableList from './components/Statistic/Table/TableList';
 
 function App() {
-
-   // const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  // const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   const isLoggedIn = true;
-  return <>
-    {isLoggedIn && <AppBar />}
-     <MainView />
-    <Currency />
-     <CategoryList />
-   <TableList/>
-  </>;
-
-
+  return (
+    <>
+      {isLoggedIn && <AppBar />}
+      <MainView />
+      <Currency />
+      <CategoryList />
+      <TableList />
+    </>
+  );
 }
 export default App;
