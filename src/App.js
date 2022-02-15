a6fa6beb312050e8fa99591d26da105f8d980922
SovaV <sova.volodymyr95@gmail.com>
2022-02-15 17:09:05 +0200

    route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router';
 import MainView from './views/MainView';
 import './styles/index.scss';
 import AppBar from './components/AppBar/AppBar';
@@ -5,17 +6,25 @@ import AppBar from './components/AppBar/AppBar';
 import Currency from './components/Currency';
 import CategoryList from './components/Statistic/Category/CategoryList';
 import TableList from './components/Statistic/Table/TableList';
+import MenuNavigation from './components/MenuNavigation/MenuNavigation';
+import Container from './components/Container/Container';
 
 function App() {
   // const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   const isLoggedIn = true;
   return (
     <>
-      {isLoggedIn && <AppBar />}
-      <MainView />
-      <Currency />
-      <CategoryList />
-      <TableList />
+      <Container>
+        {isLoggedIn && <AppBar />}
+        <MenuNavigation />
+        <Routes>
+          <Route path="/" element={<MainView />} />
+          <Route path="/currency" element={<Currency />} />
+          <Route path="/statistics" element={<CategoryList />} />
+          {/* <Route path="/statistics" element={<TableList />} /> */}
+        </Routes>
+        {/* <TableList /> */}
+      </Container>
     </>
   );
 }
---
+++ src/components/MenuNavigation/MenuNavigation.js
@@ -0,0 +1,61 @@
+import { NavLink } from 'react-router-dom';
+import Media from 'react-media';
+import sprite from '../../images/sprite.svg';
+
+export default function MenuNavigation() {
+  return (
+    <Media query="(max-width: 768px)">
+      {matches =>
+        matches ? (
+          <nav>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? 'active-style' : 'link')}
+            >
+              <svg width="44" height="44">
+                <use href={`${sprite}#icon-home`}></use>
+              </svg>
+            </NavLink>
+            <NavLink
+              to="/statistics"
+              className={({ isActive }) => (isActive ? 'active-style' : 'link')}
+            >
+              <svg width="44" height="44">
+                <use href={`${sprite}#icon-statistics`}></use>
+              </svg>
+            </NavLink>
+            <NavLink
+              to="/currency"
+              className={({ isActive }) => (isActive ? 'active-style' : 'link')}
+            >
+              <svg width="44" height="44">
+                <use href={`${sprite}#icon-currency`}></use>
+              </svg>
+            </NavLink>
+          </nav>
+        ) : (
+          <nav>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? 'active-style' : 'link')}
+            >
+              <svg width="44" height="44">
+                <use href={`${sprite}#icon-home`}></use>
+              </svg>
+              Home
+            </NavLink>
+            <NavLink
+              to="/statistics"
+              className={({ isActive }) => (isActive ? 'active-style' : 'link')}
+            >
+              <svg width="44" height="44">
+                <use href={`${sprite}#icon-statistics`}></use>
+              </svg>
+              Statistics
+            </NavLink>
+          </nav>
+        )
+      }
+    </Media>
+  );
+}
