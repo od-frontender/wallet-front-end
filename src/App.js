bf1f2eb82ce4591696deb85fe5127b5d1e894d7d
SovaV <sova.volodymyr95@gmail.com>
2022-02-18 14:39:11 +0200

    сделал lazy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Media from 'react-media';
+import { lazy, Suspense } from 'react';
 import './styles/index.scss';
 
 import AppBar from './components/AppBar';
-import MainView from './views/MainView';
-import Currency from './components/Currency';
-import TableList from './components/Statistic/Table/TableList';
 import MenuNavigation from './components/MenuNavigation/MenuNavigation';
 import Container from './components/Container/Container';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
 import NotFoundView from './components/NotFoundView/NotFoundView';
+import Spiner from '../src/components/Spinner';
+
+const MainView = lazy(() =>
+  import('./views/MainView' /* webpackChunkName: "MainView" */),
+);
+const Currency = lazy(() =>
+  import('./components/Currency' /* webpackChunkName: "Currency" */),
+);
+const TableList = lazy(() =>
+  import(
+    './components/Statistic/Table/TableList' /* webpackChunkName: "TableList" */
+  ),
+);
 
 function App() {
   // const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
@@ -19,45 +31,47 @@ function App() {
       <Container>
         {isLoggedIn && <AppBar />}
         {isLoggedIn && <MenuNavigation />}
-        <Routes>
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <MainView />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/statistics"
-            element={
-              <PrivateRoute>
-                <TableList />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/currency"
-            element={
-              <PrivateRoute>
-                <Media query="(max-width: 767px)">
-                  {matches =>
-                    matches ? (
-                      <>
-                        <Currency />
-                      </>
-                    ) : (
-                      <>
-                        <Navigate replace to="/home" />
-                      </>
-                    )
-                  }
-                </Media>
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<NotFoundView />} />
-        </Routes>
+        <Suspense fallback={<Spiner />}>
+          <Routes>
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <MainView />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/statistics"
+              element={
+                <PrivateRoute>
+                  <TableList />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/currency"
+              element={
+                <PrivateRoute>
+                  <Media query="(max-width: 767px)">
+                    {matches =>
+                      matches ? (
+                        <>
+                          <Currency />
+                        </>
+                      ) : (
+                        <>
+                          <Navigate replace to="/home" />
+                        </>
+                      )
+                    }
+                  </Media>
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<NotFoundView />} />
+          </Routes>
+        </Suspense>
       </Container>
     </>
   );
