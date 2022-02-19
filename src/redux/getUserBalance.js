f3102d68f631a9fa96ed9d12270f13c9ef42f1bb
Vladomira <juliavladomira@gmail.com>
2022-02-19 10:34:41 +0300

    corrects styles and function for balance

--- src/redux/getUserBalance.js
+++ src/redux/getUserBalance.js
@@ -1,13 +1,14 @@
 import dateFormat from 'dateformat';
 
 export function getUserBalance(payload) {
-  const data = payload.map(({ total, createdAt }) => {
-    return { total, createdAt: dateFormat(createdAt, 'mm.dd.yy h:MM:ss ') };
-  });
+  // const data = payload.map(({ total, createdAt }) => {
+  //   return { total, createdAt: dateFormat(createdAt, 'mm.dd.yy h:MM:ss ') };
+  // });
 
-  const lastData = data.sort((a, b) => {
-    return Date.parse(b.createdAt) - Date.parse(a.createdAt);
-  });
+  // data.sort((a, b) => {
+  //   return Date.parse(b.createdAt) - Date.parse(a.createdAt);
+  // });
+  const lastData = payload.sort((a, b) => a.getTime() - b.getTime());
   const result = lastData[0].total;
   return result;
 }
