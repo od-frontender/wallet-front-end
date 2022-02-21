7faf07e3ca623d4041aa9738dddb15e5555209fa
Prykhodko-Pasha <prykhodko.p@gmail.com>
2022-02-21 13:22:34 +0200

    feat: diagram markup done

--- src/components/StatisticsDiagram/StatisticsDiagram.js
+++ src/components/StatisticsDiagram/StatisticsDiagram.js
@@ -1,18 +1,22 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
 import s from './StatisticsDiagram.module.scss';
 
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const StatisticsDiagram = () => {
-  const colorsArr = [
-    '#FED057',
-    '#FFD8D0',
-    '#FD9498',
-    '#FF6596',
-    '#C5BAFF',
-    '#6E78E8',
-    '#4A56E2',
-    '#81E1FF',
-    '#24CCA7',
-    '#00AD84',
-  ];
+  //   const colorsArr = [
+  //     '#FED057',
+  //     '#FFD8D0',
+  //     '#FD9498',
+  //     '#FF6596',
+  //     '#C5BAFF',
+  //     '#6E78E8',
+  //     '#4A56E2',
+  //     '#81E1FF',
+  //     '#24CCA7',
+  //     '#00AD84',
+  //   ];
   const categoriesStats = [
     {
       category: 'basic',
@@ -55,52 +59,40 @@ const StatisticsDiagram = () => {
       total: 1000,
     },
   ];
-  const totalSum = categoriesStats.reduce((acc, obj) => {
-    return acc + obj.total;
-  }, 0);
-
-  let prevPercentage = 0;
-  const diagramBackgroundStyle = {
-    background: `conic-gradient(
-    ${categoriesStats.map(({ total }, index) => {
-      if (index === 0) {
-        return (
-          colorsArr[index] +
-          ' 0% ' +
-          ((total * 100) / totalSum).toFixed(1) +
-          '%'
-        );
-      } else {
-        prevPercentage += (categoriesStats[index - 1].total * 100) / totalSum;
-        return (
-          colorsArr[index] +
-          ' ' +
-          prevPercentage.toFixed(1) +
-          '%' +
-          ' ' +
-          ((total * 100) / totalSum + prevPercentage).toFixed(1) +
-          '%'
-        );
-      }
-    })}
-  )`,
+  const data = {
+    datasets: [
+      {
+        label: 'Total spending',
+        data: categoriesStats.map(item => item.total),
+        backgroundColor: [
+          '#FED057',
+          '#FFD8D0',
+          '#FD9498',
+          '#FF6596',
+          '#C5BAFF',
+          '#6E78E8',
+          '#4A56E2',
+          '#81E1FF',
+          '#24CCA7',
+          '#00AD84',
+        ],
+        borderWidth: 0,
+        cutout: '70%',
+      },
+    ],
   };
-  console.log('diagramBackgroundStyle :>> ', diagramBackgroundStyle);
 
   return (
-    <div className={s.my_pie_chart_container}>
-      <div className={s.my_pie_chart} style={diagramBackgroundStyle}></div>
-      {/* <div className={s.legenda}>
-        {categoriesStats.map(({ category, total }, index) => (
-          <div className={s.entry} key={index}>
-            <div
-              className={`${s.color_brown} ${s.entry_color}`}
-              style={{ backgroundColor: colorsArr[index] }}
-            ></div>
-            <div className={s.entry_text}>{category}</div>
-          </div>
-        ))}
-      </div> */}
+    <div className={s.chart_container}>
+      <Doughnut data={data} />
+      <span className={s.chart_inner_text}>
+        ₴{' '}
+        {categoriesStats
+          .reduce((acc, obj) => {
+            return acc + obj.total;
+          }, 0)
+          .toFixed(2)}
+      </span>
     </div>
   );
 };
