54c0ff3ce4d2811832ddd326d939bebb2cf5b625
Prykhodko-Pasha <prykhodko.p@gmail.com>
2022-02-22 16:02:16 +0200

    feat: add styles for login page

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,3 +1,5 @@
+import s from './Container.module.scss';
+
 export default function Container({ children }) {
-  return <div className="container">{children}</div>;
+  return <div className={s.container}>{children}</div>;
 }
--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,12 +1,15 @@
 import AppBar from '../../components/AppBar';
 import Background from '../../components/Background/Background';
+import Blur from '../../components/Blur/Blur';
 import Main from '../../components/Main';
 
 function DashboardPage() {
   return (
     <Background>
       <AppBar />
-      <Main />
+      <Blur>
+        <Main />
+      </Blur>
     </Background>
   );
 }
