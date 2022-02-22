e3e44b6c9fe2011bbf2cedf9673e8940e09fd4e5
SovaV <sova.volodymyr95@gmail.com>
2022-02-23 00:38:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,11 @@ function App() {
         <Route
           path="/"
           element={
-            isToken ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+            isToken ? (
+              <Navigate to="/dashboard/home" />
+            ) : (
+              <Navigate to="/login" />
+            )
           }
         />
         <Route
