56ce9fba8776a8df63ef0f907afdad6c741c604c
Prykhodko-Pasha <prykhodko.p@gmail.com>
2022-03-16 15:00:58 +0200

    feat: fix styles

--- src/components/Statistics/Filters/Filters.js
+++ src/components/Statistics/Filters/Filters.js
@@ -5,6 +5,8 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import * as operations from '../../../redux/statistics/statistics-operations';
 
+import s from './Filters.module.scss';
+
 const Filters = () => {
   const [month, setMonth] = useState('');
   const [year, setYear] = useState('');
@@ -41,45 +43,32 @@ const Filters = () => {
   };
 
   return (
-    <>
-      <FormControl>
-        <Select
-          sx={{
-            width: 280,
-            margin: '0 auto',
-            height: 50,
-            border: '1px solid #000000',
-            borderRadius: '30px',
-            fontFamily: 'Circe',
-            fontSize: '16px',
-            lineHeight: '24px',
-            paddingLeft: '5px',
-          }}
-          value={month}
-          onChange={onChangeMonth}
-          inputProps={{ 'aria-label': 'Without label' }}
-          displayEmpty
-          renderValue={value => {
-            return value !== '' ? months[value] : 'Month';
-          }}
-        >
-          <MenuItem
-            disabled
-            value=""
+    <div className={s.filters}>
+      <div className={s.filters__months}>
+        <FormControl sx={{ width: '100%' }}>
+          <Select
             sx={{
+              width: '100%',
+              margin: '0 auto',
               height: 50,
+              border: '1px solid #000000',
               borderRadius: '30px',
               fontFamily: 'Circe',
               fontSize: '16px',
               lineHeight: '24px',
+              paddingLeft: '5px',
+            }}
+            value={month}
+            onChange={onChangeMonth}
+            inputProps={{ 'aria-label': 'Without label' }}
+            displayEmpty
+            renderValue={value => {
+              return value !== '' ? months[value] : 'Month';
             }}
           >
-            Month
-          </MenuItem>
-          {months.map((mon, index) => (
             <MenuItem
-              key={index}
-              value={index}
+              disabled
+              value=""
               sx={{
                 height: 50,
                 borderRadius: '30px',
@@ -88,62 +77,78 @@ const Filters = () => {
                 lineHeight: '24px',
               }}
             >
-              {mon}
+              Month
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+            {months.map((mon, index) => (
+              <MenuItem
+                key={index}
+                value={index}
+                sx={{
+                  height: 50,
+                  borderRadius: '30px',
+                  fontFamily: 'Circe',
+                  fontSize: '16px',
+                  lineHeight: '24px',
+                }}
+              >
+                {mon}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
-      <FormControl>
-        <Select
-          sx={{
-            width: 280,
-            margin: '0 auto',
-            height: 50,
-            border: '1px solid #000000',
-            borderRadius: '30px',
-            fontFamily: 'Circe',
-            fontSize: '16px',
-            lineHeight: '24px',
-            marginTop: '20px',
-            paddingLeft: '5px',
-          }}
-          value={year}
-          onChange={onChangeYear}
-          inputProps={{ 'aria-label': 'Without label' }}
-          displayEmpty
-          renderValue={value => {
-            return value !== '' ? value : 'Year';
-          }}
-        >
-          <MenuItem
-            disabled
-            value=""
+      <div className={s.filters__years}>
+        <FormControl sx={{ width: '100%' }}>
+          <Select
             sx={{
+              width: '100%',
+              margin: '0 auto',
               height: 50,
+              border: '1px solid #000000',
               borderRadius: '30px',
               fontFamily: 'Circe',
               fontSize: '16px',
               lineHeight: '24px',
+              paddingLeft: '5px',
+            }}
+            value={year}
+            onChange={onChangeYear}
+            inputProps={{ 'aria-label': 'Without label' }}
+            displayEmpty
+            renderValue={value => {
+              return value !== '' ? value : 'Year';
             }}
           >
-            Year
-          </MenuItem>
-          <MenuItem
-            value={2022}
-            sx={{
-              height: 50,
-              borderRadius: '30px',
-              fontFamily: 'Circe',
-              fontSize: '16px',
-              lineHeight: '24px',
-            }}
-          >
-            2022
-          </MenuItem>
-        </Select>
-      </FormControl>
-    </>
+            <MenuItem
+              disabled
+              value=""
+              sx={{
+                height: 50,
+                borderRadius: '30px',
+                fontFamily: 'Circe',
+                fontSize: '16px',
+                lineHeight: '24px',
+              }}
+            >
+              Year
+            </MenuItem>
+            <MenuItem
+              value={2022}
+              sx={{
+                height: 50,
+                borderRadius: '30px',
+                fontFamily: 'Circe',
+                fontSize: '16px',
+                lineHeight: '24px',
+              }}
+            >
+              2022
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </div>
   );
 };
 
